8d5af8639bb65761464d43774d0f71d1284121b8
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-04-28 17:14:40 -0500

    Nevel fail (test cases)

--- tests/test_console.py
+++ tests/test_console.py
@@ -39,3 +39,8 @@ class TestConsoleDocs(unittest.TestCase):
                          "HBNBCommand class needs a docstring")
         self.assertTrue(len(HBNBCommand.__doc__) >= 1,
                         "HBNBCommand class needs a docstring")
+
+
+if __name__ == "__main__":
+    if os.getenv("HBNB_TYPE_STORAGE") != "db":
+        unittest.main()
