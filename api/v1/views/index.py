cefebd831dfae9eb96a635011df00610ef5c4a7d
juanmarcos <juanmarcoscabezas@gmail.com>
2020-04-29 22:31:03 +0000

    Fixed

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,6 @@
 from api.v1.views import app_views
 from flask import jsonify
 
-@app_views.route('/status'. strict_slashes=False, methods=['GET'])
+@app_views.route('/status', strict_slashes=False, methods=['GET'])
 def status():
     return jsonify({'status': 'ok'}), 200
