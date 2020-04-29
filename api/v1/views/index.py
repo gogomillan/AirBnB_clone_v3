93b1fab435368e1b09647f1c259d2957558333e5
juanmarcos <juanmarcoscabezas@gmail.com>
2020-04-29 22:39:15 +0000

    Fixing pep8 errors

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+"""
+Route Index
+"""
 
 from api.v1.views import app_views
 from flask import jsonify
+
 
 @app_views.route('/status', strict_slashes=False, methods=['GET'])
 def status():
