b8b2fd1dc17a86c3775def66c9c9c03cefcf8058
juanmarcos <juanmarcoscabezas@gmail.com>
2020-04-29 22:57:08 +0000

    Fixing storage import

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,4 +1,10 @@
 from flask import Blueprint
+from models.state import State
+from models.amenity import Amenity
+from models.city import City
+from models.user import User
+from models.place import Place
+from models.review import Review
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
