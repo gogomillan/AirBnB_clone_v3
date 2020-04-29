d2f779fc7f5c188053a33378e59d890fe7eda97c
juanmarcos <juanmarcoscabezas@gmail.com>
2020-04-29 22:20:28 +0000

    Updating app views

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,14 +1,15 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 
+from flask import Flask
 from models import storage
 from api.v1.views import app_views
-from flask import Flask
+
 app = Flask(__name__)
 
 app.register_blueprint(app_views)
 
 @app.teardown_appcontext
-def close_session(exc):
+def teardown(self):
     """Calls close session storage"""
     storage.close()
 
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,6 @@
 from api.v1.views import app_views
 from flask import jsonify
 
-@app_views.route('/status')
+@app_views.route('/status'. strict_slashes=False, methods=['GET'])
 def status():
-    return jsonify({'status': 'ok'})
+    return jsonify({'status': 'ok'}), 200
