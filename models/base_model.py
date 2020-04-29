51a8fefe3254feeb9c475f58a834530e179b858d
juanmarcos <juanmarcoscabezas@gmail.com>
2020-04-29 21:21:05 +0000

    Fixing base model

--- models/base_model.py
+++ models/base_model.py
@@ -34,15 +34,20 @@ class BaseModel:
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
                     setattr(self, key, value)
-            if self.id is None:
-                setattr(self, 'id', str(uuid.uuid4()))
-            if self.created_at is None:
-                self.created_at = datetime.now()
-            if self.updated_at is None:
-                self.updated_at = datetime.now()
+            if kwargs.get("created_at", None) and type(self.created_at) is str:
+                self.created_at = datetime.strptime(kwargs["created_at"], time)
+            else:
+                self.created_at = datetime.utcnow()
+            if kwargs.get("updated_at", None) and type(self.updated_at) is str:
+                self.updated_at = datetime.strptime(kwargs["updated_at"], time)
+            else:
+                self.updated_at = datetime.utcnow()
+            if kwargs.get("id", None) is None:
+                self.id = str(uuid.uuid4())
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.now()
+            self.created_at = self.updated_at = datetime.utcnow()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """String representation of the BaseModel class"""
