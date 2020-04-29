3b669d82f3a729a61bdd568b22869aa808be2c88
juanmarcos <juanmarcoscabezas@gmail.com>
2020-04-29 22:58:38 +0000

    Fixing pep8 warning

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,10 +7,12 @@ from api.v1.views import app_views, Place, City, Amenity, Review, State, User
 from flask import jsonify
 from models import storage
 
+
 @app_views.route('/status', strict_slashes=False, methods=['GET'])
 def status():
     """Return API status"""
     return jsonify({"status": "OK"}), 200
+
 
 @app_views.route('/stats', strict_slashes=False, methods=['GET'])
 def stats():
