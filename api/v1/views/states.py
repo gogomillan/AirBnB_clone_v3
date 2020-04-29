2ac4848c9c974387b347794d7adba16633773a0a
juanmarcos <juanmarcoscabezas@gmail.com>
2020-04-29 23:44:26 +0000

    Get state by ID

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """
-Route 
+Route
 """
 
 from api.v1.views import app_views, State
-from flask import jsonify
+from flask import jsonify, abort
 from models import storage
 
 
@@ -14,3 +14,13 @@ def all_states():
     states = storage.all(State)
     states = [state.to_dict() for state in states.values()]
     return jsonify(states), 200
+
+
+@app_views.route('/states/<id>', strict_slashes=False, methods=['GET'])
+def get_state(id):
+    """Return state by id"""
+    state = storage.get(State, id)
+    if state:
+        state = state.to_dict()
+        return jsonify(state), 200
+    return abort(404)
