30dbb9f413d3852a0efd76d7658612739a7278fa
juanmarcos <juanmarcoscabezas@gmail.com>
2020-04-30 00:35:00 +0000

    Create state is working

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -4,7 +4,7 @@ Route
 """
 
 from api.v1.views import app_views, State
-from flask import jsonify, abort
+from flask import jsonify, abort, request
 from models import storage
 
 
@@ -35,3 +35,18 @@ def delete_state(id):
         storage.save()
         return jsonify({}), 200
     return abort(404)
+
+
+@app_views.route('/states', strict_slashes=False, methods=['POST'])
+def create_state():
+    """Creates a new state"""
+    body = request.get_json(silent=True)
+    if body is None:
+        return jsonify({'error': 'Not a JSON'}), 400
+    if 'name' in body:
+        new_state = State(**body)
+        storage.new(new_state)
+        storage.save()
+        return jsonify(new_state.to_dict()), 200
+    else:
+        return jsonify({'error': 'Missing name'}), 400
