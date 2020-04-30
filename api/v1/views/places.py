d653781a0df1ac6985df0efcfa87f0851b68957c
juanmarcos <juanmarcoscabezas@gmail.com>
2020-04-30 03:15:19 +0000

    Fixing places routes

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -47,9 +47,12 @@ def delete_place(id):
     return abort(404)
 
 
-@app_views.route('/places', strict_slashes=False, methods=['POST'])
-def create_place():
+@app_views.route('/cities/<id>/places', strict_slashes=False, methods=['POST'])
+def create_place(id):
     """Creates a new place"""
+    city_exist = storage.get(City, id)
+    if city_exist is None:
+        return abort(404)
     body = request.get_json(silent=True)
     if body is None:
         return jsonify({'error': 'Not a JSON'}), 400
@@ -61,6 +64,7 @@ def create_place():
     if 'name' not in body:
         return jsonify({'error': 'Missing name'}), 400
     new_place = Place(**body)
+    setattr(new_place, 'city_id', id)
     storage.new(new_place)
     storage.save()
     return jsonify(new_place.to_dict()), 201
@@ -79,7 +83,8 @@ def update_place(id):
             if user_exist is None:
                 return abort(404)
         for key in body:
-            if key != 'id' and key != 'created_at' and key != 'updated_at':
+            if key != 'id' and key != 'created_at' and key != 'updated_at'\
+                    and key != 'user_id' and key != 'city_id':
                 setattr(place, key, body[key])
         place.save()
         return jsonify(place.to_dict()), 200
