facdc0a846297549617c4da27a3ba5f20fb518a4
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-05-05 19:00:43 -0500

    Add documentation to reviews API

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -10,7 +10,44 @@ from models import storage
 @app_views.route('/places/<place_id>/reviews', strict_slashes=False,
                  methods=['GET'])
 def get_reviews_places(place_id):
-    """Returns place by id"""
+    """ Method for the "/places/<place_id>/reviews" path GET
+    Returns reviews by place
+    ---
+    tags:
+      - Review
+    parameters:
+      - name: place_id
+        in: path
+        type: string
+        required: true
+        description: The ID of Place, try 279b355e-ff9a-4b85-8114-6db7ad2a4cd2
+    responses:
+      200:
+        description: An array of Review objects
+        examples:
+          [
+            {
+              "__class__":"Review",
+              "created_at":"2017-03-25T02:17:07.000000",
+              "id":"3f54d114-582d-4dab-8559-f0682dbf1fa6",
+              "place_id":"279b355e-ff9a-4b85-8114-6db7ad2a4cd2",
+              "text":"Really nice place and really nice people. Secluded. Everything was clean. Very convenient. You will love it.",
+              "updated_at":"2017-03-25T02:17:07.000000",
+              "user_id":"887dcd8d-d5ee-48de-9626-73ff4ea732fa"
+            },
+            {
+              "__class__":"Review",
+              "created_at":"2017-03-25T02:17:07.000000",
+              "id":"78388983-b5a5-47af-8ebd-e7f005540831",
+              "place_id":"279b355e-ff9a-4b85-8114-6db7ad2a4cd2",
+              "text":"Perfect for our one night stay in New Orleans. Clean & comfortable.",
+              "updated_at":"2017-03-25T02:17:07.000000",
+              "user_id":"7231eaa1-400f-4cb5-a867-f5eba8adbb81"
+            }
+          ]
+      404:
+        description: When place_id not found
+    """
     place = storage.get(Place, place_id)
     if place:
         reviews = [review.to_dict() for review in place.reviews]
@@ -20,7 +57,33 @@ def get_reviews_places(place_id):
 
 @app_views.route('/reviews/<review_id>', strict_slashes=False, methods=['GET'])
 def get_review(review_id):
-    """Returns place by id"""
+    """ Method for the "/reviews/<review_id>" path GET
+    Returns Review by id
+    ---
+    tags:
+      - Review
+    parameters:
+      - name: review_id
+        in: path
+        type: string
+        required: true
+        description: The ID of State, try 3f54d114-582d-4dab-8559-f0682dbf1fa6
+    responses:
+      200:
+        description: A State object
+        examples:
+          {
+            "__class__":"Review",
+            "created_at":"2017-03-25T02:17:07.000000",
+            "id":"3f54d114-582d-4dab-8559-f0682dbf1fa6",
+            "place_id":"279b355e-ff9a-4b85-8114-6db7ad2a4cd2",
+            "text":"Really nice place and really nice people. Secluded. Everything was clean. Very convenient. You will love it.",
+            "updated_at":"2017-03-25T02:17:07.000000",
+            "user_id":"887dcd8d-d5ee-48de-9626-73ff4ea732fa"
+          }
+      404:
+        description: When review_id not found
+    """
     review = storage.get(Review, review_id)
     if review:
         review = review.to_dict()
